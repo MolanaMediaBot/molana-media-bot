f146a7398ee236c6b5dfe35994ca7eaf86498997
MolanaMediaBot <mohsensheibany@gmail.com>
2026-06-01 02:20:58 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,4 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-import os
+print("BOT STARTED SUCCESSFULLY")
 
-TOKEN = os.getenv("BOT_TOKEN")
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-await update.message.reply_text("ربات فعال است")
-
-app = Application.builder().token(TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+while True:
+pass

012f9139291a84924f1f0d6b8b64b958532c2a2a
MolanaMediaBot <mohsensheibany@gmail.com>
2026-06-01 02:58:29 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,10 @@ import os
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-await update.message.reply_text("ربات فعال است")
+   await update.message.reply_text("ربات فعال است")
 
 app = Application.builder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
+
 app.run_polling()

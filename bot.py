8485c1c9957e7e97040963a60346e7ba4ff3dab1
MolanaMediaBot <mohsensheibany@gmail.com>
2026-05-31 23:32:58 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,30 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 import os
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "به ربات رسانه مولانا خوش آمدید.\n\nویدئوهای آموزشی به زودی اینجا قرار می‌گیرند."
-    )
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "به ربات رسانه مولانا خوش آمدید.\n\nیک ویدئو برای من ارسال کنید."
+    )
+
+async def get_video_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.video:
+        file_id = update.message.video.file_id
+
+        print("=" * 50)
+        print("VIDEO FILE ID:")
+        print(file_id)
+        print("=" * 50)
+
+        await update.message.reply_text(
+            f"Video ID:\n{file_id}"
+        )
+
 app = Application.builder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.VIDEO, get_video_id))
 
 app.run_polling()
